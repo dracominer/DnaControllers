fad3e35dc978c9c1372d17f5ae7d6c6188330b7f
dracominer <dracominer947@gmail.com>
2017-09-09 16:53:22 -0500

    Added better layer of abstraction

---
+++ DNA Controllers/src/dna/TestGeneral.java
@@ -0,0 +1,72 @@
+package dna;
+
+import javax.swing.JFrame;
+
+import dna.controllers.DNAAbstractController;
+import dna.controllers.DNALoadControllers;
+import dna.controllers.DPadDir;
+import dna.controllers.XBoxPad;
+
+/**
+ * this is a main testing class for showing that the system works. I will change it from time to time to test different features.
+ * */
+@SuppressWarnings("serial")
+public class TestGeneral extends JFrame implements Runnable {
+
+	public static void main(String[] args) {
+		print("Game started");
+		new TestGeneral().start();
+	}
+
+	protected DNAAbstractController gamepad;
+
+	public TestGeneral() {
+		super("Gamepad test");
+		this.setSize(400, 300);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+
+	public void start() {
+		new Thread(this).start();
+	}
+
+	public void run() {
+		setUpController();
+		if (gamepad == null) {
+			System.err.println("No controllers found!");
+			return;
+		}
+		while (true) {
+			gamepad.poll();
+			runTest();
+		}
+	}
+
+	protected void setUpController() {
+		gamepad = DNALoadControllers.loadOneXboxController();
+		if (gamepad != null) print("Using gamepad: " + gamepad.getName());
+		gamepad.printControllerData();
+	}
+
+	protected void runTest() {
+		for (int i = 0; i < gamepad.getRegisteredButtons(); i++) {
+			if (i == XBoxPad.BTN_DPad) continue;
+			if (gamepad.getButton(i)) print("Btn_" + i + " is pressed");
+		}
+		for (int i = 0; i < gamepad.getRegisteredAxes(); i++) {
+			if (Math.abs(gamepad.getAxis(i)) > 0.25f) print("Axis_" + i + " is in use");
+		}
+		DPadDir dir = gamepad.getDPadDir(XBoxPad.BTN_DPad);
+		if (!dir.equals(DPadDir.None)) {
+			print("DPad: " + dir.toString());
+		}
+	}
+
+	private static void print(String x) {
+		System.out.println(x);
+	}
+
+}
--- DNA Controllers/src/dna/controllers/XBoxPad.java
+++ DNA Controllers/src/dna/controllers/XBoxPad.java
@@ -1,24 +1,11 @@
 package dna.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import net.java.games.input.Component;
-import net.java.games.input.Component.Identifier;
 import net.java.games.input.Controller;
-import net.java.games.input.Controller.PortType;
-import net.java.games.input.Controller.Type;
-import net.java.games.input.EventQueue;
-import net.java.games.input.Rumbler;
 
 /**
  * after this class is initialized, it can handle all input from an xbox gamepad
  * */
-public class XBoxPad {
-
-	private Controller controller;
-	private Component[] buttons;
-	private Component[] axes;
+public class XBoxPad extends DNAAbstractController {
 
 	public static final int BTN_A = 0;
 	public static final int BTN_B = 1;
@@ -39,112 +26,31 @@ public class XBoxPad {
 	public static final int Axis_RightX = 3;
 
 	public XBoxPad(Controller controller) {
-		this.controller = controller;
-		Component[] sub = getComponents();
-		List<Component> btns = new ArrayList<Component>();
-		List<Component> axs = new ArrayList<Component>();
-		for (int i = 0; i < sub.length; i++) {
-			Component part = sub[i];
-			System.out.println("Found xbox part: " + part.getName() + " [" + part.getIdentifier() + "] (analog? " + part.isAnalog() + ")");
-			if (!part.isAnalog()) {
-				btns.add(part);
-			} else {
-				axs.add(part);
-			}
-		}
-		buttons = new Component[btns.size()];
-		axes = new Component[axs.size()];
-		System.out.println("Found " + buttons.length + " buttons and " + axes.length + " axes");
-		for (int i = 0; i < btns.size(); i++) {
-			buttons[i] = btns.get(i);
-		}
-		for (int i = 0; i < axs.size(); i++) {
-			axes[i] = axs.get(i);
-		}
+		super(controller);
 	}
 
-	public int getRegisteredButtons() {
-		return buttons.length;
+	public boolean getBtnA() {
+		return getButton(BTN_A);
 	}
 
-	public int getRegisteredAxes() {
-		return axes.length;
+	public boolean getBtnB() {
+		return getButton(BTN_B);
 	}
 
-	public boolean getButton(int id) {
-		return getButtonRaw(id) == 1.0f;
+	public boolean getBtnX() {
+		return getButton(BTN_X);
 	}
 
-	public DPadDir getDPadDir(int id) {
-		float val = getButtonRaw(id);
-		if (val <= 0) return DPadDir.None;
-		if (val <= 0.125) return DPadDir.NW;
-		if (val <= 0.25) return DPadDir.N;
-		if (val <= 0.375) return DPadDir.NE;
-		if (val <= 0.5) return DPadDir.E;
-		if (val <= 0.625) return DPadDir.SE;
-		if (val <= 0.75) return DPadDir.S;
-		if (val <= 0.875) return DPadDir.SW;
-		if (val <= 1.0) return DPadDir.W;
-		return DPadDir.None;
+	public boolean getBtnY() {
+		return getButton(BTN_Y);
 	}
 
-	public float getButtonRaw(int id) {
-		if (id < 0 || id >= buttons.length) return 0.0f;
-		return buttons[id].getPollData();
+	public boolean getBumperLeft() {
+		return getButton(BTN_BumperLeft);
 	}
 
-	public float getAxis(int id) {
-		if (id < 0 || id >= axes.length) return 0.0f;
-		return axes[id].getPollData();
-	}
-
-	public Controller getController() {
-		return controller;
-	}
-
-	public Controller[] getControllers() {
-		return controller.getControllers();
-	}
-
-	public Type getType() {
-		return controller.getType();
-	}
-
-	public Component[] getComponents() {
-		return controller.getComponents();
-	}
-
-	public Component getComponent(Identifier id) {
-		return controller.getComponent(id);
-	}
-
-	public Rumbler[] getRumblers() {
-		return controller.getRumblers();
-	}
-
-	public boolean poll() {
-		return controller.poll();
-	}
-
-	public void setEventQueueSize(int size) {
-		controller.setEventQueueSize(size);
-	}
-
-	public EventQueue getEventQueue() {
-		return controller.getEventQueue();
-	}
-
-	public PortType getPortType() {
-		return controller.getPortType();
-	}
-
-	public int getPortNumber() {
-		return controller.getPortNumber();
-	}
-
-	public String getName() {
-		return controller.getName();
+	public boolean getBumperRight() {
+		return getButton(BTN_BumperRight);
 	}
 
 }
